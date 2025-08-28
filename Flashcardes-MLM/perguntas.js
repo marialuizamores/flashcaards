370dd9f3cd6ce4aafe855fd5cc3ceddb631aff1d
marialuizamores <maria.luiza.mores@escola.pr.gov.br>
2025-08-28 13:58:53 -0300

    Add files via upload

---
+++ Flashcardes-MLM/perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'desenho',
+    'Qual é a cor que a barbie mais usa ',
+    'rosa'
+)
+
+criaCartao(
+    'matematica',
+    'quanto é 20+80',
+    '100,'
+)
+
+criaCartao(
+    'geografia',
+    'qual é a capital do Paraná?',
+    'Curitiba'
+)
+criaCartao(
+    'matematica',
+    'qual é a raiz quadrada de 25',
+    '5'
+)
+criaCartao(
+    'matematica',
+    'quanto é o valor de Pi',
+    '3,14'
+)
+criaCartao(
+    'quimica',
+    ' qual é a formula fisica da agua',
+    'H2O'
+)
+criaCartao(
+    'matematica',
+    'qual é a raiz quadrada de 9',
+    ' 3 dança'
+)
+criaCartao(
+    'geografia',
+    'qual é a capital do Brasil',
+    'Brasilia'
+)
+criaCartao(
+    'matematica',
+    ' quanto é 50+55',
+    '105'
+)
